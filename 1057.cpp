5bde886304926f450e1d5869b62e4edafd40020f
gleb-kov <kovg16@gmail.com>
2020-03-29 10:56:17 +0300

    update 1057

--- 1057.cpp
+++ 1057.cpp
@@ -1,75 +1,54 @@
 #include <iostream>
+#include <array>
 #include <vector>
 #include <cmath>
-#include <string>
-#include <map>
-#include <iomanip>
-#include <queue>
-#include <algorithm>
-using namespace std;
-typedef long long int ll;
-typedef long double ld;
 
-int k, del;
+std::array<std::array<int64_t, 32>, 32> c_table;
 
-vector<vector<ll>> c_table;
+int64_t solve_prefix(int32_t n, size_t k, int64_t b) {
+    std::vector<int64_t> digit;
+    size_t nonzero = 0;
+    while (n) {
+        digit.push_back(n % b);
+        if (n % b != 0) nonzero++;
+        n /= b;
+    }
+    int64_t res = (nonzero == k);
+    if (k > digit.size()) return 0;
 
-ll get_answ(int n) {
-	vector<int> b;
-	ll res = 0;
-	int mn= 0, cnt = 0, rn = n;
-	while (rn) {
-		b.push_back(rn % del);
-		if (rn % del != 0) mn++;
-		cnt++;
-		rn /= del;
-	}
-	if (mn == k) res++;
+    for (size_t i = k; i < digit.size(); i++) res += c_table[i - 1][k - 1];
+    digit.pop_back();
 
-	if (k > cnt) return 0;
-
-	for (ll i = k; i < cnt; i++) res += c_table[i - 1][k - 1];
-	//cout << "prec " << res << endl;
-	b.erase(--b.end());
-	cnt--;
-	reverse(b.begin(), b.end());
-
-	int cur = 1;
-
-	for (int i = 0; i < cnt && cur <= k; i++) {
-		if (b[i] > 0) {
-			res += c_table[cnt - i - 1][k - cur];
-			cur++;
-		}
-	}
-	return res;
+    size_t cnt = digit.size(), cur = 1;
+    for (size_t i = cnt; i > 0 && cur <= k; i--) {
+        if (digit[i - 1] == 0) continue;
+        res += c_table[i - 1][k - cur];
+        cur++;
+    }
+    return res;
 }
 
-ll get_b(ll m, ll cur, int step, int col) {
-	if (step == 10 || col >= k || cur > m) {
-		if (cur <= m && col == k) return 1;
-		return 0;
-	}
-	ll t = get_b(m, cur, step + 1, col) + get_b(m, cur + pow(10, step), step + 1, col + 1);
-	return t;
+int64_t solve_prefix10(int64_t n, size_t k, int64_t cur = 0, size_t step = 0, size_t filled = 0) {
+    return (step == 10 || filled >= k || cur > n ?
+            (int64_t)(cur <= n && filled == k) :
+            solve_prefix10(n, k, cur, step + 1, filled) +
+            solve_prefix10(n, k, cur + pow(10, step), step + 1, filled + 1));
 }
 
-int main()
-{
-	c_table.resize(32, vector<ll>(32, 0));
-	c_table[0][0] = 1;
-	for (int i = 1; i < 32; i++) {
-		c_table[i][0] = 1;
-		for (int j = 1; j < 32; j++) {
-			c_table[i][j] = c_table[i - 1][j] + c_table[i - 1][j - 1];
-		}
-	}
-	int a, b;
-	cin >> a >> b >> k >> del;
-	if (del != 10) {
-		cout << get_answ(b) - get_answ(a - 1);
-	}
-	else {
-		cout << get_b(b, 0, 0, 0)-get_b(a - 1, 0, 0, 0);
-	}
+int main() {
+    c_table[0].fill(0);
+    c_table[0][0] = 1;
+    for (size_t i = 1; i < 32; i++) {
+        c_table[i][0] = 1;
+        for (size_t j = 1; j < 32; j++)
+            c_table[i][j] = c_table[i - 1][j] + c_table[i - 1][j - 1];
+    }
+    size_t k;
+    int64_t x, y, b, result = 0;
+    std::cin >> x >> y >> k >> b;
+    result = (b == 10 ?
+              solve_prefix10(y, k) - solve_prefix10(x - 1, k) :
+              solve_prefix(y, k, b) - solve_prefix(x - 1, k, b));
+    std::cout << result;
 }
+
