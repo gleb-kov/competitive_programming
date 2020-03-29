733c5f81142520422c7ec1d01e2d3ec1e02c76a5
gleb-kov <kovg16@gmail.com>
2020-03-29 11:03:38 +0300

    auto code style[2]

--- 1067.cpp
+++ 1067.cpp
@@ -2,10 +2,11 @@
 #include <map>
 #include <vector>
 #include <iostream>
+
 using namespace std;
 
 struct stackvertex {
-	map<string, stackvertex*> nextword;
+    map<string, stackvertex *> nextword;
 };
 
 string t, s;
@@ -14,41 +15,41 @@ stackvertex *p = new stackvertex;
 stackvertex *cur;
 
 void push() {
-	cur = p;
-	t = "";
-	for (int i = 0; i < s.length(); i++) {
-		if ((int)s[i] == 92) {
-			if (cur->nextword[t] == NULL) {
-				cur->nextword[t] = new stackvertex;
-			}
-			cur = cur->nextword[t];
-			t = "";
-		} else t += s[i];
-	}
-	if (cur->nextword[t] == NULL) {
-		cur->nextword[t] = new stackvertex;
-	}
+    cur = p;
+    t = "";
+    for (int i = 0; i < s.length(); i++) {
+        if ((int) s[i] == 92) {
+            if (cur->nextword[t] == NULL) {
+                cur->nextword[t] = new stackvertex;
+            }
+            cur = cur->nextword[t];
+            t = "";
+        } else t += s[i];
+    }
+    if (cur->nextword[t] == NULL) {
+        cur->nextword[t] = new stackvertex;
+    }
 }
 
-void print(stackvertex* &cur, string now, int amount = 0) {
-	for (int i = 0; i < amount - 1; i++) cout << " ";
-	if (now != "") cout << now << endl;
-	amount++;
+void print(stackvertex *&cur, string now, int amount = 0) {
+    for (int i = 0; i < amount - 1; i++) cout << " ";
+    if (now != "") cout << now << endl;
+    amount++;
 
-	for (auto it = cur->nextword.begin(); it != cur->nextword.end(); it++) {
-		if (it->second != NULL) {
-			print(it->second, it->first, amount);
-		}
-	}
+    for (auto it = cur->nextword.begin(); it != cur->nextword.end(); it++) {
+        if (it->second != NULL) {
+            print(it->second, it->first, amount);
+        }
+    }
 }
 
 int main() {
-	int n;
-	cin >> n;
-	for (int i = 0; i<n; i++) {
-		cin >> s;
-		push();
-	}
-	print(p, "");
-	return 0;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        push();
+    }
+    print(p, "");
+    return 0;
 }
--- 1331.cpp
+++ 1331.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cmath>
 #include <iomanip>
+
 using namespace std;
 typedef long long int ll;
 typedef long double ld;
@@ -10,43 +11,42 @@ typedef long double ld;
 const ld pi = acos(-1.00);
 #define Epsilon 1E-10
 struct info {
-	ld x, y, z;
+    ld x, y, z;
 };
 
-double arccos(double X)
-{
-	if (abs(X) < Epsilon) return pi / 2;
-	if (X <= -1) return pi;
-	if (X >= 1) return 0;
-	if (X > 0) return atan(sqrt(1 - X * X) / X);
-	return pi + atan(sqrt(1 - X * X) / X);
+double arccos(double X) {
+    if (abs(X) < Epsilon) return pi / 2;
+    if (X <= -1) return pi;
+    if (X >= 1) return 0;
+    if (X > 0) return atan(sqrt(1 - X * X) / X);
+    return pi + atan(sqrt(1 - X * X) / X);
 }
 
-int main()
-{
-	int n, m;
-	ld r,w,l,x,y,z, minpo, curpo, curcos;
-	cin >> n >> m >> r;
-	vector<info> places(m);
-	for (int i = 0; i < m; i++) {
-		cin >> w >> l;
-		places[i].z = sin(pi*w / 180.0);
-		places[i].x = cos(pi*l / 180.0) * cos(pi*w/180.0);
-		places[i].y = sin(pi*l / 180.0) * cos(pi*w/180.0);
-	}
+int main() {
+    int n, m;
+    ld r, w, l, x, y, z, minpo, curpo, curcos;
+    cin >> n >> m >> r;
+    vector<info> places(m);
+    for (int i = 0; i < m; i++) {
+        cin >> w >> l;
+        places[i].z = sin(pi * w / 180.0);
+        places[i].x = cos(pi * l / 180.0) * cos(pi * w / 180.0);
+        places[i].y = sin(pi * l / 180.0) * cos(pi * w / 180.0);
+    }
 
-	for (int i = 0; i < n; i++) {
-		cin >> w >> l;
-		x = cos(pi*l / 180.0) * cos(pi*w / 180.0);
-		z = sin(pi*w / 180.0);
-		y = sin(pi*l / 180.0) * cos(pi*w / 180.0);
+    for (int i = 0; i < n; i++) {
+        cin >> w >> l;
+        x = cos(pi * l / 180.0) * cos(pi * w / 180.0);
+        z = sin(pi * w / 180.0);
+        y = sin(pi * l / 180.0) * cos(pi * w / 180.0);
 
-		minpo = 1e10;
-		for (int j = 0; j < m; j++) {
-			curpo = (x - places[j].x)*(x - places[j].x) + (y - places[j].y)*(y - places[j].y) + (z - places[j].z)*(z - places[j].z);
-			minpo = min(minpo, curpo);
-		}
-		curcos = 1 - minpo / 2;
-		cout << fixed << setprecision(2) << arccos(curcos)*r << endl;
-	}
+        minpo = 1e10;
+        for (int j = 0; j < m; j++) {
+            curpo = (x - places[j].x) * (x - places[j].x) + (y - places[j].y) * (y - places[j].y) +
+                    (z - places[j].z) * (z - places[j].z);
+            minpo = min(minpo, curpo);
+        }
+        curcos = 1 - minpo / 2;
+        cout << fixed << setprecision(2) << arccos(curcos) * r << endl;
+    }
 }
